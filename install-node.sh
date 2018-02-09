89145c924c7062c71db743daf8c0ac78921c7cf4
Márcio Coelho <marciojc@gmail.com>
2018-02-09 11:42:31 +0000

    Fix commands

--- install-node.sh
+++ install-node.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-- curl -sL https://deb.nodesource.com/setup_7.x | bash -
-- curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+curl -sL https://deb.nodesource.com/setup_7.x | bash -
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 
 # Remove ubuntu's default yarn and install the nodejs yarn
-- apt-get remove cmdtest && apt remove yarn
+apt-get remove cmdtest && apt remove yarn
 
 # Install dependencies
-- curl -sS https://getcomposer.org/installer | php
-- apt-get install git nodejs -yqq
+curl -sS https://getcomposer.org/installer | php
+apt-get install git nodejs -yqq
+npm install -g yarn
--- install-php.sh
+++ install-php.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Install dependencies
-- apt-get install libcurl4-gnutls-dev libicu-dev libmcrypt-dev libvpx-dev libjpeg-dev libpng-dev libxpm-dev zlib1g-dev libfreetype6-dev libxml2-dev libexpat1-dev libbz2-dev libgmp3-dev libldap2-dev unixodbc-dev libpq-dev libsqlite3-dev libaspell-dev libsnmp-dev libpcre3-dev libtidy-dev -yqq
-- pecl install mcrypt-1.0.1
-- docker-php-ext-enable mcrypt
+apt-get install libcurl4-gnutls-dev libicu-dev libmcrypt-dev libvpx-dev libjpeg-dev libpng-dev libxpm-dev zlib1g-dev libfreetype6-dev libxml2-dev libexpat1-dev libbz2-dev libgmp3-dev libldap2-dev unixodbc-dev libpq-dev libsqlite3-dev libaspell-dev libsnmp-dev libpcre3-dev libtidy-dev -yqq
+pecl install mcrypt-1.0.1
+docker-php-ext-enable mcrypt
 
 # Install php extensions
-- docker-php-ext-install mbstring pdo_mysql curl json intl gd xml zip bz2 opcache
+docker-php-ext-install mbstring pdo_mysql curl json intl gd xml zip bz2 opcache
