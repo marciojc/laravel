e03e693a6e231f59ce69ddbd73a048ebdc3414f9
Márcio Coelho <marciojc@gmail.com>
2018-02-09 11:41:06 +0000

    Add new docker root

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,12 @@
-FROM php:alpine
+FROM php:latest
 LABEL maintainer="marciojcc <marcio@gen.pt>"
 
-RUN apk update && apk upgrade && apk add bash git openssh
+RUN apt-get update -yqq && apt-get install -my wget gnupg
 
 # Install PHP extensions
 ADD install-php.sh /usr/sbin/install-php.sh
 RUN /usr/sbin/install-php.sh
 
 # Download and install NodeJS
-ENV NODE_VERSION 8.9.4
 ADD install-node.sh /usr/sbin/install-node.sh
 RUN /usr/sbin/install-node.sh
-RUN npm i -g yarn
-
-RUN apk update && apk upgrade && apk add python2 && apk upgrade
--- install-php.sh
+++ install-php.sh
@@ -1,55 +1,9 @@
 #!/bin/sh
 
-apk add freetds freetype icu libintl libldap libjpeg libmcrypt libpng libpq libwebp
+# Install dependencies
+- apt-get install libcurl4-gnutls-dev libicu-dev libmcrypt-dev libvpx-dev libjpeg-dev libpng-dev libxpm-dev zlib1g-dev libfreetype6-dev libxml2-dev libexpat1-dev libbz2-dev libgmp3-dev libldap2-dev unixodbc-dev libpq-dev libsqlite3-dev libaspell-dev libsnmp-dev libpcre3-dev libtidy-dev -yqq
+- pecl install mcrypt-1.0.1
+- docker-php-ext-enable mcrypt
 
-TMP="curl-dev \
-    freetds-dev \
-    freetype-dev \
-    gettext-dev \
-    icu-dev \
-    jpeg-dev \
-    libmcrypt-dev \
-    libpng-dev \
-    libwebp-dev \
-    libxml2-dev \
-    openldap-dev \
-    postgresql-dev"
-apk add $TMP
-
-# Configure extensions
-docker-php-ext-configure gd --with-jpeg-dir=usr/ --with-freetype-dir=usr/ --with-webp-dir=usr/
-docker-php-ext-configure ldap --with-libdir=lib/
-docker-php-ext-configure pdo_dblib --with-libdir=lib/
-
-# Download mongo extension
-/usr/local/bin/pecl download mongodb && \
-    tar -C /usr/src/php/ext -xf mongo*.tgz && \
-    rm mongo*.tgz && \
-    mv /usr/src/php/ext/mongo* /usr/src/php/ext/mongodb
-
-docker-php-ext-install \
-    curl \
-    exif \
-    gd \
-    gettext \
-    intl \
-    ldap \
-    mongodb \
-    pdo_dblib \
-    pdo_mysql \
-    pdo_pgsql \
-    xmlrpc \
-    zip
-
-# Download trusted certs 
-mkdir -p /etc/ssl/certs && update-ca-certificates
-
-# Install composer
-php -r "readfile('https://getcomposer.org/installer');" | php && \
-   mv composer.phar /usr/bin/composer && \
-   chmod +x /usr/bin/composer
-
-apk del $TMP
-
-# Set timezone
-# RUN echo America/Maceio > /etc/timezone && dpkg-reconfigure -f noninteractive tzdata
+# Install php extensions
+- docker-php-ext-install mbstring pdo_mysql curl json intl gd xml zip bz2 opcache
